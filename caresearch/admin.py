1d4b718ec9316bebe85c86f5e6b73966eaff8c16
RozWelch <rozwelch@gmail.com>
2023-01-21 11:44:39 +0000

    Add approve actions to admin for care providers and comments

--- caresearch/admin.py
+++ caresearch/admin.py
@@ -9,18 +9,25 @@ class CareProviderComments(SummernoteModelAdmin):
     search_fields = ['careprovider', 'comment']
     summernote_fields = ('comment')
     list_filter = ('approved', 'date_created')
+    actions = ['approve_comments']
 
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
 
 @admin.register(CareProvider)
 class CareProvider(SummernoteModelAdmin):
 
-    list_display = ('business_name', 'main_contact_name', 'county', 'provider_approved_status')
-    search_fields = ['business_name', 'main_contact_name', 'county',]
+    list_display = ('business_name', 'main_contact_name', 'county', 'email', 'provider_approved_status')
+    search_fields = ['business_name', 'main_contact_name', 'email', 'county',]
     list_filter = ('provider_approved_status', 'business_name')
+    actions = ['approve_careprovider']
+
+    def approve_careprovider(self, request, queryset):
+        queryset.update(provider_approved_status=True)
 
 @admin.register(CareSeeker)
 class CareSeeker(SummernoteModelAdmin):
 
-    list_display = ('first_name','second_name', 'county')
-    search_fields = ['second_name','first_name', 'county']
+    list_display = ('first_name','second_name', 'email', 'county')
+    search_fields = ['second_name','first_name', 'email', 'county']
     list_filter = ('second_name', 'careseeker_username' )
