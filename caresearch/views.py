b0d8b764f800e5770248abf3b21b5f9ffa39e197
RozWelch <rozwelch@gmail.com>
2023-01-22 12:31:07 +0000

    Add view for care provider details

--- caresearch/views.py
+++ caresearch/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import CareProvider
 
 
@@ -8,3 +8,23 @@ class CareProviderList(generic.ListView):
     queryset = CareProvider.objects.filter(provider_approved_status=1).order_by('business_name')
     template_name = 'careproviders_list.html'
     paginate_by = 6
+
+class CareProviderDetail(View):
+
+    def get(self, request, business_name, *args, **kwargs):
+        queryset = CareProvider.objects.fileter(provider_approved_status)
+        business = get_object_or_404(queryset, business_name=business_name)
+        careprovidercomments = business.careprovidercomments(approved=True).order_by('-date_created')
+        likes = False
+        if business.like.filter(id=self.request.user.id).exists():
+            likes = True
+
+        return render(
+            request, 
+            "careprovider_detail.html"
+            {
+                "business": business,
+                "careprovidercomments": careprovidercomments,
+                "likes": likes
+            },
+            )
