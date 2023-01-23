1c48c47723ad0ae634ce8e005e969e6510f3eece
RozWelch <rozwelch@gmail.com>
2023-01-23 21:00:40 +0000

    Install django allauth

--- carehub/urls.py
+++ carehub/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('caresearch.urls'), name='caresearch_urls'),
+    path('accounts/', include('allauth.urls')),
 ]
