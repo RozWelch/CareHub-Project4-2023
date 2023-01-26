537f7023326dde7f1472e2044719984913cb0864
RozWelch <rozwelch@gmail.com>
2023-01-26 13:17:13 +0000

    Add crispy form for comments if user is authenticated

--- carehub/settings.py
+++ carehub/settings.py
@@ -148,5 +148,3 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
--- caresearch/forms.py
+++ caresearch/forms.py
@@ -1,6 +1,7 @@
 from .models import CareProviderComments
 from django import forms
 
+
 class CareProviderCommentsForm(forms.ModelForm):
     class Meta:
         model = CareProviderComments
