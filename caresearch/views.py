c1496385696a04a89760401dd9f377602a0a3fc0
RozWelch <rozwelch@gmail.com>
2023-01-26 10:21:20 +0000

    Fix careprovider detail view, correct variable in careprovider detail view

--- caresearch/views.py
+++ caresearch/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from django.views.generic import DetailView
-from .models import CareProvider
+from .models import CareProvider, CareProviderComments
 from .forms import CareProviderCommentsForm
+
 
 def IndexPage(request):
     return render(request, "index.html")
+
 
 class CareProviderList(generic.ListView):
     model = CareProvider
@@ -13,25 +15,24 @@ class CareProviderList(generic.ListView):
     template_name = 'careproviders_list.html'
     paginate_by = 6
 
-class CareProviderDetail(DetailView):
-    model = CareProvider
-    template_name = 'careprovider_detail.html'
+
+class CareProviderDetail(View):
 
     def get(self, request, pk, *args, **kwargs):
         queryset = CareProvider.objects.filter(provider_approved_status=1)
         post = get_object_or_404(queryset, pk=pk)
-        careprovidercomments = post.careprovidercomments.filter(approved=True).order_by('-date_created')
+        comments = post.careprovider_comments.filter(approved=True).order_by('-date_created')
         likes = False
         if post.likes.filter(id=self.request.user.id).exists():
             likes = True
 
         return render(
-            request, 
+            request,
             "careprovider_detail.html",
             {
-                "business": business,
-                "careprovidercomments": careprovidercomments,
+                "post": post,
+                "comments": comments,
                 "likes": likes,
-                "comment_form": CommentForm()
+                "comment_form": CareProviderCommentsForm()
             },
             )
