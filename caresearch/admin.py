ab3490779f8d7da4c573d2cb7ac3e3559356e792
RozWelch <rozwelch@gmail.com>
2023-01-30 10:11:54 +0000

    Remove unnecessary fields from Care Seeker model

--- caresearch/admin.py
+++ caresearch/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import CareProvider, CareProviderComments, CareSeeker
 from django_summernote.admin import SummernoteModelAdmin
 
+
 @admin.register(CareProviderComments)
 class CareProviderComments(SummernoteModelAdmin):
 
@@ -14,20 +15,28 @@ class CareProviderComments(SummernoteModelAdmin):
     def approve_comments(self, request, queryset):
         queryset.update(approved=True)
 
+
 @admin.register(CareProvider)
 class CareProvider(SummernoteModelAdmin):
 
-    list_display = ('business_name', 'type_of_care', 'main_contact_name', 'county', 'email', 'provider_approved_status')
-    search_fields = ['business_name', 'type_of_care', 'main_contact_name', 'email', 'county',]
-    list_filter = ('provider_approved_status', 'type_of_care','business_name')
+    list_display = (
+        'business_name', 'type_of_care', 'main_contact_name',
+        'county', 'email', 'provider_approved_status'
+        )
+    search_fields = [
+        'business_name', 'type_of_care',
+        'main_contact_name', 'email', 'county',
+        ]
+    list_filter = ('provider_approved_status', 'type_of_care', 'business_name')
     actions = ['approve_careprovider']
 
     def approve_careprovider(self, request, queryset):
         queryset.update(provider_approved_status=True)
 
+
 @admin.register(CareSeeker)
 class CareSeeker(SummernoteModelAdmin):
 
-    list_display = ('first_name','second_name', 'email', 'county')
-    search_fields = ['second_name','first_name', 'email', 'county']
-    list_filter = ('second_name', 'careseeker_username' )
+    list_display = ('first_name', 'second_name', 'email', 'county')
+    search_fields = ['second_name', 'first_name', 'email', 'county']
+    list_filter = ('second_name', 'careseeker_username')
