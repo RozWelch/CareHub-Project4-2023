289063d1f8ae31007d9d95d959abdf4a55442ce0
RozWelch <rozwelch@gmail.com>
2023-02-09 13:52:58 +0000

    Fix pep8 errors in admin py file

--- carehub/settings.py
+++ carehub/settings.py
@@ -114,7 +114,7 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME':
+        'NAME': 
         'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
--- caresearch/admin.py
+++ caresearch/admin.py
@@ -20,8 +20,9 @@ class CareProviderComments(SummernoteModelAdmin):
 class CareProvider(SummernoteModelAdmin):
 
     list_display = (
-       'careprovider_username', 'business_name', 'type_of_care', 'main_contact_name',
-        'county', 'email', 'description', 'provider_approved_status'
+       'careprovider_username', 'business_name', 'type_of_care',
+       'main_contact_name', 'county', 'email', 'description',
+       'provider_approved_status'
         )
     search_fields = [
         'business_name', 'type_of_care',
