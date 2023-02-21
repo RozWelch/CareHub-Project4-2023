af4c4dce490a25fd36db65033c23577b18e4b873
RozWelch <rozwelch@gmail.com>
2023-02-21 13:32:15 +0000

    Fix validator errors in python files

--- caresearch/forms.py
+++ caresearch/forms.py
@@ -8,18 +8,6 @@ class CareProviderCommentsForm(forms.ModelForm):
         fields = ('comment',)
 
 
-# class ProviderForm(forms.ModelForm):
-#     class Meta:
-#         model = CareProvider
-#         fields = [
-#             'business_name',
-#             'type_of_care',
-#             'main_contact_name', 'address_line_1',
-#             'county', 'phone_number', 'email',
-#             'disabled_parking', 'provider_image',
-#             ]
-
-
 class ProviderForm(forms.ModelForm):
     class Meta:
         model = CareProvider
